47b67456e8cbf484b19cdf39e5947c2de7016e25
ticatac <lior.keshet@gmail.com>
2016-07-11 16:05:39 +0300

    version2

---
+++ app/src/main/java/com/clm/lister/SettingsActivity.java
@@ -0,0 +1,50 @@
+package com.clm.lister;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Set;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+
+        final ArrayList<String> arrayList= new ArrayList<>();
+        arrayList.add("Send Email");
+
+        arrayList.add("Set Time");
+        arrayList.add("Set Date");
+
+        // attach to list view
+        final ListView settingsLV=(ListView) findViewById(R.id.settingsLV);
+        ArrayAdapter<String> arrayAdapter=new ArrayAdapter<String>(this,R.layout.options_list_item_2,R.id.settingTV,arrayList);
+        settingsLV.setAdapter(arrayAdapter);
+        settingsLV.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(SettingsActivity.this,arrayList.get(position),Toast.LENGTH_SHORT).show();
+                // menu
+
+
+            }
+        });
+
+
+
+    }
+    public void onBackPressed() {
+        Toast.makeText(SettingsActivity.this,"Back Pressed - Are You Sure??",Toast.LENGTH_SHORT).show();
+        finish();
+        //super.onBackPressed();
+    }
+
+}
---
+++ app/src/main/java/com/clm/lister/buttons.java
@@ -0,0 +1,15 @@
+package com.clm.lister;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class buttons extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_buttons);
+
+
+    }
+}
