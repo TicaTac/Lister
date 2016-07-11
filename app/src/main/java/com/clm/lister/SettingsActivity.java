cf4cca8fea2471dd1d5b88c9c1515030d507c0df
ticatac <lior.keshet@gmail.com>
2016-07-11 17:07:35 +0300

    version 4 - DatePicker Dialog problem

--- app/src/main/java/com/clm/lister/SettingsActivity.java
+++ app/src/main/java/com/clm/lister/SettingsActivity.java
@@ -1,10 +1,12 @@
 package com.clm.lister;
 
+import android.app.DatePickerDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.DatePicker;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -33,6 +35,25 @@ public class SettingsActivity extends AppCompatActivity {
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 Toast.makeText(SettingsActivity.this,arrayList.get(position),Toast.LENGTH_SHORT).show();
                 // menu
+                switch (arrayList.get(position)){
+                    case "Set Time": // open set time dialog
+
+                     //   break;
+                    case "Set Date":// open set date dialog
+
+                        DatePickerDialog dpd=new DatePickerDialog(SettingsActivity.this, DatePickerDialog.OnDateSetListener(){
+                            
+
+                        },2013, 0 ,0);
+
+                        dpd.show();
+
+                        //   break;
+                    default:
+                        Toast.makeText(SettingsActivity.this,"Default",Toast.LENGTH_SHORT).show();
+                        break;
+
+                }
 
 
             }
